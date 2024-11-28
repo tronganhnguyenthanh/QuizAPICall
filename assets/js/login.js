496522fa65c31123dc2d6054dc4983e53a94adf3
Phuong Nguyen <tronganhnguyen222@gmail.com>
2024-11-28 22:08:47 +0700

    [UPDATE][FE] build login system

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+function checkLoginSystem(){
+ let username = document.querySelector("#inputUsername").value
+ let password = document.querySelector("#inputPassword").value
+ if(username === "" || password === ""){
+  document.querySelector("#message").innerHTML = "<p class='text-center text-danger'>Please enter all fields</p>"
+  return
+ }else{
+   localStorage.setItem("username", username)
+   localStorage.setItem("password", password)
+   window.location.href = "index.html"
+ }
+}
